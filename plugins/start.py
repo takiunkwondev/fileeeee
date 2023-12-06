35e197a3a9daa93934da3cdbf02004bb45c3f2a2
Takievil <138558442+Takievilkun@users.noreply.github.com>
2023-12-06 20:24:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,7 +90,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                     [
-                        InlineKeyboardButton("⚡𝙹𝚘𝚒𝚗⚡", url="https://t.me/Dynamite_Network")                        
+                        InlineKeyboardButton("⚡𝙹𝚘𝚒𝚗⚡", url="https://t.me/Anime_Hindi_Dub_Official")                        
                     ],
                     [
                         InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃", callback_data = "about"),
